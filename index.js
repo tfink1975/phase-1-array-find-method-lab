cb2b42b6727b1f3f9dce35938a270616263cb558
Tim <tim.fink519@gmail.com>
2022-08-31 13:29:01 -0400

    commit tree b8112cf7b233f74d0c4d2e6585067d5c3adfad81

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(obj => obj.result === 'W');
+    console.log(win);
+    if(win) {
+        return win.year;
+    }
+};
+
+// console.log(record.find(superbowlWin));
